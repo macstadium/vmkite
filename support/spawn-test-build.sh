6ae0bcc7335250fd8c2e20863473dbcbaefd5283
Lachlan Donald <lachlan@strongwater.com.au>
2017-04-02 19:55:31 +1000

    Use new build api

--- support/spawn-test-build.sh
+++ support/spawn-test-build.sh
@@ -4,13 +4,21 @@ set -e
 set -o pipefail
 set -u
 
-curl https://api.buildkite.com/v2/organizations/macstadium/pipelines/vmkite-test/builds \
-  --silent \
+export org_slug=macstadium
+export pipeline_slug=vmkite-test
+
+curl -X POST "https://api.buildkite.com/v2/organizations/${org_slug}/pipelines/${pipeline_slug}/builds" \
   --show-error \
   --fail \
-  -X POST \
   -H "Authorization: Bearer $VMKITE_BUILDKITE_API_TOKEN" \
-  -F "commit=HEAD" \
-  -F "branch=master" \
-  -F "message=test build" \
-  > /dev/null
+  -d @- << JSON
+{
+  "commit": "HEAD",
+  "branch": "master",
+  "message": "Testing vmkite build :rocket: :llama:",
+  "meta_data": {
+    "vmkite-vmdk": "${VMKITE_SOURCE_PATH}",
+    "vmkite-guestid": "${VMKITE_VM_GUEST_TYPE}"
+  }
+}
+JSON
